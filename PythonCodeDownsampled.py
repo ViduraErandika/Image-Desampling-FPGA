06902ddc63782dbe7207944af5224916f1fde702
udani1997 <71671260+udani1997@users.noreply.github.com>
2022-07-01 18:20:55 +0530

    Add files via upload
    Processor Design Until 01/07/2022

--- PythonCodeDownsampled.py
+++ PythonCodeDownsampled.py
@@ -9,10 +9,20 @@ im = cv2.imread("bird.jpg",0)         #Output the image in gray scale mode
 im=np.pad(im,1, mode='constant')     #Zero pad for the image
 im=im.flatten(order='C')             #Convert the image into 1D array
 
+imageArrayhexfile = open ('imageArrayhexfile.txt' , 'w' )
+imageArrayhexfile.write('FF')
+imageArrayhexfile.write("\n")
+
+for l in im :
+    hexVal = hex(l)
+    strHex = str(hexVal)
+    finalhex = strHex[2:]
+    imageArrayhexfile.write(str(finalhex))
+    imageArrayhexfile.write("\n")
+
 print(len(im))
 
 i = ((1+256+1)+1)
-total = 0
 j=1
 while(i<((256*256)-(256-2)-1)):      #Filter out high pass frequecies by passing through kernal
     total = im[i-1+256+2]
@@ -25,9 +35,9 @@ while(i<((256*256)-(256-2)-1)):      #Filter out high pass frequecies by passing
     total = total + 2*im[i-256-2]
     total = total + im[i+1-256-2]
     im[i] = total/16
-    total = total+2
+    i = i+2
     j=j+1
-    if j>256//2:
+    if j==129:
         j=1
         i=i+256+4
 
@@ -40,7 +50,7 @@ while i<((256+2)*(256+2))-(256+2)-1:
     a.append(im[i])
     i=i+2
     j=j+1
-    if j>256//2:
+    if j==128:
         j=1
         i=i+256+4
         downsampled.append(a)
