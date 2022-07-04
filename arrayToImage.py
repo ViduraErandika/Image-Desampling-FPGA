3707ec0c5b082c65e6f1c47b50b2223c92049818
Vidura Karunarathn <viduraerandika@gmail.com>
2022-07-05 03:03:02 +0530

    array to image updated

---
+++ arrayToImage.py
@@ -0,0 +1,22 @@
+import numpy as np
+from cv2 import waitKey
+from PIL import Image
+
+desampled_file=open("Desampled.txt",'r')
+lines = desampled_file.readlines()
+y = []
+x = []
+j=1
+for i in lines  :
+    y.append(int(i))
+    j = j+1
+    if j==129:
+        j=1
+        x.append(y)
+        y=[]
+
+
+downsampled=np.array(x)
+print(downsampled)
+gr_im= Image.fromarray(downsampled)
+gr_im.convert("L").save('downsampled_bird_from_processor.png')
